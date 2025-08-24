32c053b7733b4203e65940632f7c43d7b7d77d58
JulioVS <julioer@me.com>
2025-08-24 17:54:28 -0300

    Set button handler, update team and bundle ID

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -18,9 +18,21 @@ class ViewController: UIViewController {
         super.viewDidLoad()
 
         // Set initial dice faces at runtime
+        // The current Xcode version suggests this code
+        // to set the image attribute:
         diceImageView1.image = UIImage(imageLiteralResourceName: "DiceSix")
         diceImageView2.image = UIImage(imageLiteralResourceName: "DiceTwo")
     }
     
+    // Interface Builder Actions allow us to resppond to
+    // UI element's events
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        print("Button got tapped!")
+
+        // This time I typed '#imageLiteral(' to make the
+        // image picker appear just as in Angela's video
+        diceImageView1.image = #imageLiteral(resourceName: "DiceFour")
+        diceImageView2.image = #imageLiteral(resourceName: "DiceFour")
+    }
 }
 
